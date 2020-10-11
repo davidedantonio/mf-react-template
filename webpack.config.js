0d49267ae12aefd31d6aa47a8d8124702e915370
davidedantonio <davide.dantonio@webeetle.com>
2020-10-11 10:09:12 +0200

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,12 @@ module.exports = {
           presets: ['@babel/preset-react'],
         },
       },
+      {
+        test: /\.m?js/,
+        resolve: {
+          fullySpecified: false
+        }
+      }
     ],
   },
   plugins: [
